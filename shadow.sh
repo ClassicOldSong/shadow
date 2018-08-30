7a75db483bab258eac309f513c312fdb0a88a0be
Yukino Song <syqlds@126.com>
2018-08-30 17:34:31 +0800

    Refactor

--- shadow.sh
+++ shadow.sh
@@ -7,12 +7,17 @@ RED="\033[0;31m"
 NC="\033[0m"
 
 ROOT_LIST=`ls /`
-IGNORE_LIST="dev proc sys"
-CLEAR_LIST="/mnt /run /boot /var/run"
-SHADOW_ROOT="$PWD/.shadow"
+KEEP_SHADOW_ENV=${KEEP_SHADOW_ENV:=""}
+IGNORE_LIST=${IGNORE_LIST:="dev proc sys"}
+CLEAR_LIST=${CLEAR_LIST:="/mnt /run /boot /var/run"}
+SHADOW_IMG=${SHADOW_IMG:="shadow"}
+SHADOW_PERFIX=${SHADOW_PERFIX:="SHADOW-"}
+SHADOW_DIR=${SHADOW_DIR:=".shadow"}
+SHADOW_ROOT="$PWD/$SHADOW_DIR"
 SHADOW_MERGED="$SHADOW_ROOT/merged"
 SHADOW_UPPER="$SHADOW_ROOT/upper"
 SHADOW_WORK="$SHADOW_ROOT/workdir"
+SHADOW_LOCK="$SHADOW_ROOT/.shadowlock"
 SHADOW_TMP="$SHADOW_MERGED/tmp"
 SHADOW_DOCKER="$SHADOW_MERGED/var/lib/docker"
 
@@ -22,30 +27,6 @@ SHADOW_EXISTS=$?
 cEcho () {
 	echo -e "${CYAN}[SHADOW]${NC} $*"
 }
-
-IMG_NAME="shadow"
-docker images | grep $IMG_NAME > /dev/null
-
-if [ "$?" != "0" ]; then
-	BUILD_DIR="/tmp/shadow_build"
-
-	cEcho "Shadow image \"$IMG_NAME\" not found, trying to build..."
-
-	mkdir -p $BUILD_DIR
-	echo -e "FROM scratch\nCMD /bin/sh\n" > $BUILD_DIR/Dockerfile
-
-	pushd $BUILD_DIR > /dev/null
-	docker build -t $IMG_NAME . > /dev/null
-	popd > /dev/null
-
-	rm -rf $BUILD_DIR
-	cEcho "Build complete"
-fi
-
-if [[ "$KEEP_SHADOW_ENV" == "" && "$SHADOW_EXISTS" == "0" ]]; then
-  cEcho "Overlay $SHADOW_ROOT already exists! exiting..."
-  exit 1
-fi
 
 # contains method from:
 # https://stackoverflow.com/questions/8063228/how-do-i-check-if-a-variable-exists-in-a-list-in-bash
@@ -67,43 +48,147 @@ clearDirs () {
 	done
 }
 
-if [ "$SHADOW_EXISTS" != "0" ]; then
-	cEcho "Preparing temp directories at $SHADOW_ROOT ..."
-	mkdir -p $SHADOW_MERGED $SHADOW_UPPER $SHADOW_WORK
-	cEcho "Done"
+prepareEnv () {
+	if [ "$SHADOW_EXISTS" != "0" ]; then
+		cEcho "Preparing temp directories at $SHADOW_ROOT ..."
+		mkdir -p $SHADOW_MERGED $SHADOW_UPPER $SHADOW_WORK
+		cEcho "Done"
+	fi
+
+	cEcho "Mounting shadow directories"
+	mount -t overlay overlay -olowerdir=/,upperdir=$SHADOW_UPPER,workdir=$SHADOW_WORK $SHADOW_MERGED
+	cEcho "Overlay mounted at $SHADOW_ROOT"
+	mount -t tmpfs tmpfs -orw,nosuid,nodev $SHADOW_TMP
+	cEcho "Tmp mounted"
+	mount -t tmpfs tmpdocker -osize=2g $SHADOW_DOCKER
+	cEcho "Docker mounted"
+	cEcho "This mounts an empty fs to $SHADOW_DOCKER, otherwise will cause problems"
+
+	if [ "SHADOW_EXISTS" != "0" ]; then
+		clearDirs $CLEAR_LIST
+	fi
+}
+
+detatched () {
+	docker top `cat $SHADOW_LOCK` > /dev/null 2> /dev/null
+	if [ "$?" == "0" ]; then
+		cEcho "Container detatched, re-enter with \"sudo shadow\" in $PWD"
+		cEcho "If you would like to keep the shadow env after another attach, add \"KEEP_SHADOW_ENV\" to the environment virables when attaching."
+		exit
+	fi
+
+	cEcho "Container stoped"
+	rm -f $SHADOW_LOCK
+	umount -l $SHADOW_DOCKER
+	cEcho "Docker unmounted"
+	umount -l $SHADOW_TMP
+	cEcho "Tmp unmounted"
+	umount -l $SHADOW_MERGED
+	cEcho "Overlay unmounted"
+
+	if [ "$KEEP_SHADOW_ENV" == "" ]; then
+		rm -rf $SHADOW_ROOT
+		cEcho "Temp directory cleared, shadow exited"
+	else
+		cEcho "Shadow exited, shadow env saved"
+		cEcho "If you would like to remove the shadow env after another run, do not add \"KEEP_SHADOW_ENV\" to the environment virables when starting."
+	fi
+}
+
+attachDocker () {
+	echo -e "${GREEN}>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>${NC}"
+	docker attach `cat $SHADOW_LOCK`
+	echo -e "\n${GREEN}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<${NC}"
+}
+
+startDocker () {
+	cEcho "Starting shadow environment..."
+	cEcho "${YELLOW}Detatch with sequence Ctrl+P, Ctrl+Q${NC}"
+
+	SHADOW_NAME=$SHADOW_PERFIX$RANDOM
+	echo $SHADOW_NAME > $SHADOW_LOCK
+
+	echo -e "${GREEN}>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>${NC}"
+	docker run --name $SHADOW_NAME --rm --privileged --hostname $SHADOW_NAME -it `extractVolume $ROOT_LIST` $SHADOW_IMG "$@"
+	echo -e "\n${GREEN}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<${NC}"
+}
+
+# Args
+# Show version
+if [ "$1" == "--version" ]; then
+	echo "Shadow v0.1"
+	exit
 fi
 
-cEcho "Mounting shadow directories"
-mount -t overlay overlay -olowerdir=/,upperdir=$SHADOW_UPPER,workdir=$SHADOW_WORK $SHADOW_MERGED
-cEcho "Overlay mounted at $SHADOW_ROOT"
-mount -t tmpfs tmpfs -orw,nosuid,nodev $SHADOW_TMP
-cEcho "Tmp mounted"
-mount -t tmpfs tmpdocker -osize=2g $SHADOW_DOCKER
-cEcho "Docker mounted"
-cEcho "This mounts an empty fs to $SHADOW_DOCKER, otherwise will cause problems"
-
-if [ "SHADOW_EXISTS" != "0" ]; then
-	clearDirs $CLEAR_LIST
+# Show help
+if [ "$1" == "--help" ]; then
+	echo "Usage: shadow [CMD...]"
+	echo
+	echo "  --version  Print out version of Shadow"
+	echo "  --clear    Clear shadow env in current directory"
+	echo "  --help     Print out this message"
+	echo
+	echo "Read more at https://github.com/ClassicOldSong/shadow/blob/master/README.md"
+	echo
+	echo "Report bugs at https://github.com/ClassicOldSong/shadow/issues/new"
+	exit
 fi
 
-cEcho "Starting shadow environment..."
-cEcho "${YELLOW}DO NOT DETATCH THIS CONTAINER INSTANCE${NC}"
-echo -e "${GREEN}>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>${NC}"
-docker run --rm --privileged --hostname SHADOW-$HOSTNAME -it `extractVolume $ROOT_LIST` $IMG_NAME "$@"
+# Clear current shadow env
+if [ "$1" == "--clear" ]; then
+	if [ "$SHADOW_EXISTS" != "0" ]; then
+		cEcho "Shadow not exists, exit"
+		exit
+	fi
 
-echo -e "${GREEN}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<${NC}"
-cEcho "Container stoped"
-umount -l $SHADOW_DOCKER
-cEcho "Docker unmounted"
-umount -l $SHADOW_TMP
-cEcho "Tmp unmounted"
-umount -l $SHADOW_MERGED
-cEcho "Overlay unmounted"
+	if [  -f "$SHADOW_LOCK" ]; then
+		cEcho "Stopping container..."
+		docker kill `cat $SHADOW_LOCK` > /dev/null
+	fi
+	detatched
+	exit
+fi
 
-if [ "$KEEP_SHADOW_ENV" == "" ]; then
-	rm -rf $SHADOW_ROOT
-	cEcho "Temp directory cleared, shadow exited"
+# Check if this is a shadow already
+echo $HOSTNAME | grep "^$SHADOW_PERFIX" > /dev/null
+if [ "$?" == "0" ]; then
+	cEcho "Already inside a shadow, exit"
+	exit 1
+fi
+
+# Build shadow image if not exists
+docker images | grep $SHADOW_IMG > /dev/null
+if [ "$?" != "0" ]; then
+	BUILD_DIR="/tmp/shadow_build"
+
+	cEcho "Shadow image \"$SHADOW_IMG\" not found, trying to build..."
+
+	mkdir -p $BUILD_DIR
+	echo -e "FROM scratch\nCMD /bin/sh\n" > $BUILD_DIR/Dockerfile
+
+	pushd $BUILD_DIR > /dev/null
+	docker build -t $SHADOW_IMG . > /dev/null
+	popd > /dev/null
+
+	rm -rf $BUILD_DIR
+	cEcho "Build complete"
+fi
+
+# Ask whether to attach if shadow is running
+if [ -f "$SHADOW_LOCK" ]; then
+	# Prompt script from:
+	# https://stackoverflow.com/questions/226703/how-do-i-prompt-for-yes-no-cancel-input-in-a-linux-shell-script
+	cEcho "Shadow in this dir is already running, attatch?"
+	select yn in "Yes" "No"; do
+	    case $yn in
+	        Yes ) attachDocker; break;;
+	        No ) exit;;
+	    esac
+	done
 else
-	cEcho "Shadow exited, shadow env saved"
-	cEcho "To restore a saved shadow env, set \"KEEP_SHADOW_ENV\" again on next run."
+	prepareEnv
+	startDocker "$@"
 fi
+
+# Run detatch after all kinds of attachment
+detatched
